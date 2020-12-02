0fe1531e6a086748539d6c85f09a95885b9577c7
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-12-01 19:42:44 -0500

    completed routes

--- meme-post/routes/api/likedMeme.js
+++ meme-post/routes/api/likedMeme.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const likedMemesController = require();
+const likedMemesController = require("../../controllers/likeMemeController");
 
 // mathces with "/api/creates"
-router.route("/create")
+router.route("/liked")
     .post(likedMemesController.create);
