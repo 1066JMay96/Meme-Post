e41dde923260b62f2bad69b4a5ab4de0cf522e6f
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-12-10 22:26:52 -0500

    deploying to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
   
 import React from "react";
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-// import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
@@ -17,19 +16,20 @@ import LikedMemePage from "./pages/LikedMemePage";
 function App() {
   return (
       <div>
-        <Router basename="/Meme-Post">
           <AuthProvider>
-          <Navbar />
-                <Route path="/signup" component={Signup} />
+          <Router>
+            <Navbar />
+                <Route exact path ="/" component={HomePage} />
+                <Route exact path="/signup" component={Signup} />
                 <PrivateRoute exact path ="/profile" component={Profile} />
-                <Route path="/login" component={Login} />
-                <Route path ="/forgot-password" component={ForgotPassword} />
-                <Route path="/update_profile" component={UpdateProfile} />
-                <Route path ="/homepage" component={HomePage} />
-                <Route path ="/create" component={CreateMemePage} />
-                <Route path="/liked" component={LikedMemePage} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path ="/forgot-password" component={ForgotPassword} />
+                <Route exact path="/update_profile" component={UpdateProfile} />
+                <Route exact path ="/homepage" component={HomePage} />
+                <Route exact path ="/create" component={CreateMemePage} />
+                <Route exact path="/liked" component={LikedMemePage} />
+            </Router>
           </AuthProvider>
-        </Router>
       </div>
   );
 }
